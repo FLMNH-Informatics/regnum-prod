277e1f5aeaed30a5f14df4a1bec70ab7b813432c
dellch <chrisdell@gmail.com>
2018-12-13 13:32:26 -0800

    added warning for duplicate name, adjusted styling

--- public/javascripts/new_submission.js
+++ public/javascripts/new_submission.js
@@ -2,8 +2,7 @@ jQuery(document).ready(function () {
     function Submission(submission) {
         function Submitter(user){
             var self = this;
-            self.first_name = user.first_name;
-            self.last_name = user.last_name;
+            self.fullname = user.first_name + ' ' + user.last_name;
             self.email = user.email;
         }
 
@@ -15,6 +14,7 @@ jQuery(document).ready(function () {
     function NewSubmissionViewModel() {
         var self = this;
         self.existing_submissions = ko.observableArray([]);
+        self.acceptDuplicate = ko.observable(false);
         self.new_submission = {
             name:           ko.observable(""),
             'opt-out':        ko.observable(false),
@@ -25,15 +25,23 @@ jQuery(document).ready(function () {
             return !(self.new_submission['opt-out']()) || (self.new_submission.reason().trim().length > 0);
         };
 
+        self.duplicateValid = function(){
+            return ( self.existing_submissions().length === 0 || self.acceptDuplicate() );
+        };
+
+        self.nameValid = function() {
+            return self.new_submission.name().trim().length > 0;
+        };
+
         self.isValid = ko.computed(function() {
-            return (self.optOutValid() && self.new_submission.name().trim().length > 0);
+            return self.optOutValid()
+                && self.nameValid()
+                && self.duplicateValid();
         }, this);
 
         self.checkSubmission = function(){
-            debugger;
             jQuery.getJSON("my_submission/check_name", self.new_submission)
                 .done(function(data){
-                    debugger;
                     if (data.length !== 0){
                         var existing_submissions = jQuery.map(data, function(submission){ return new Submission(submission) });
                         self.existing_submissions(existing_submissions);
@@ -53,41 +61,4 @@ jQuery(document).ready(function () {
     }
 
     ko.applyBindings(new NewSubmissionViewModel());
-
-//     jQuery('#opt-out').click(function (event) {
-//         var t = jQuery('#opt-out-table');
-//         if (event.target.checked === true) {
-//             t.slideDown('fast');
-//         } else {
-//             t.slideUp();
-//         }
-//     })
-//
-//     jQuery('#create_button').click(function (event) {
-//         event.preventDefault()
-//         var alt = ''
-//         if (jQuery('#name').val().length < 1) {
-//             alt += 'Name required. '
-//         }
-//         if (jQuery('#opt-out').prop('checked') === true && jQuery('#reason').val().length < 3) {
-//             alt += 'Opt-out reason required.'
-//         }
-//
-//         if (alt.length > 0) {
-//             alert(alt)
-//         } else {
-//             var serialized = jQuery(event.target.form).serialize();
-//             debugger;
-//             jQuery.get('my_submission/check_name', serialized, function(response){
-//                 debugger;
-//                 'hi';
-//             })
-//             debugger;
-//             'hi';
-//             // jQuery.post('/save', serialized, function (res) {
-//             //     document.location.href = '/my_submission/' + res.submission_id;
-//             // }, 'json')
-//             //event.target.form.submit()
-//         }
-//     })
 });
