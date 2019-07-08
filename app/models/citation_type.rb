81e43826b22a21621d040b44671764ef7faa471f
dellch <chrisdell@gmail.com>
2019-07-07 18:52:46 -0700

    adds citation_types table with model

---
+++ app/models/citation_type.rb
@@ -0,0 +1,3 @@
+class CitationType < ApplicationRecord
+
+end
---
+++ db/migrate/20190708011138_create_citation_type_table.rb
@@ -0,0 +1,12 @@
+class CreateCitationTypeTable < ActiveRecord::Migration[5.2]
+  def change
+    if ActiveRecord::Base.connection.table_exists? 'citation_types'
+      drop_table :citation_types
+    end
+    create_table :citation_types do |t|
+      t.string :citation_type
+    end
+
+    %w(book book_section journal).each{|citation_type|  CitationType.create citation_type: citation_type }
+  end
+end
