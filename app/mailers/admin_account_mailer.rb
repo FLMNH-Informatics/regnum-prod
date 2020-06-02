087133fe1ef747063a4ec70aefc29c8c785d3b4b
dellch <chrisdell@gmail.com>
2020-06-01 22:40:56 -0700

    movin new mailer to own file

---
+++ app/mailers/admin_account_mailer.rb
@@ -0,0 +1,17 @@
+
+
+class AdminAccountMailer < ActionMailer::Base
+  default from: "do-not-reply@phyloregnum.org",
+          to: -> { User.admin_user_emails }
+
+  def account_created(user)
+    @user = user
+    mail(subject: "New account created on Regnum")
+  end
+
+  def account_activated(user)
+    @user = user
+    mail(subject: "Account activated on Regnum")
+  end
+
+end
