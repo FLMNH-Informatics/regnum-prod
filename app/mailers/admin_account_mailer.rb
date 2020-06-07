105280d819166f3b975925fccfc0610dcc517575
dellch <chrisdell@gmail.com>
2020-06-07 16:56:53 -0700

    PHY-1 updates logic when signing up with duplicat email to remember fields when returning user to sign up screen.  Adds to in AdminAccountMailer

--- app/mailers/admin_account_mailer.rb
+++ app/mailers/admin_account_mailer.rb
@@ -1,17 +1,16 @@
 
 
 class AdminAccountMailer < ActionMailer::Base
-  default from: "do-not-reply@phyloregnum.org",
-          to: -> { User.admin_user_emails }
+  default from: "do-not-reply@phyloregnum.org"
 
   def account_created(user)
     @user = user
-    mail(subject: "New account created on Regnum")
+    mail(to: User.admin_user_emails, subject: "New account created on Regnum")
   end
 
   def account_activated(user)
     @user = user
-    mail(subject: "Account activated on Regnum")
+    mail(to: User.admin_user_emails, subject: "Account activated on Regnum")
   end
 
 end
